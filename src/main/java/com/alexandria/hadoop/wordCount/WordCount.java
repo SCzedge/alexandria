6665b52c5c96033ce9a7b1da601e48a02421fe01
sczedge <sczedge@gmail.com>
2021-03-10 20:35:39 +0900

    hadoop wordcounter not done

--- src/main/java/com/alexandria/hadoop/wordCount/WordCount.java
+++ src/main/java/com/alexandria/hadoop/wordCount/WordCount.java
@@ -1,5 +1,17 @@
 package com.alexandria.hadoop.wordCount;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCount {
 
+	
+	
 }
