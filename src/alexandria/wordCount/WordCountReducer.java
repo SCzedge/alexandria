c440e95dd266264400a82691b0b207c94468eec5
SCzedge <sczedge@gmail.com>
2021-03-14 04:10:53 +0900

    nothing changed

--- src/alexandria/wordCount/WordCountReducer.java
+++ src/alexandria/wordCount/WordCountReducer.java
@@ -12,10 +12,13 @@ public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritab
 
 	protected void reduce(Text key, Iterable<IntWritable> values,
 			Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+		
 		int sum = 0;
+		
 		for (IntWritable val : values) {
 			sum += val.get();
 		}
+		
 		result.set(sum);
 		
 		context.write(key, result);
