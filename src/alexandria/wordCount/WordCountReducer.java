0963577fa5c11be997c4a19c0dcd193d2c058055
SCzedge <sczedge@gmail.com>
2021-03-15 00:59:53 +0900

    dd

--- src/alexandria/wordCount/WordCountReducer.java
+++ src/alexandria/wordCount/WordCountReducer.java
@@ -7,20 +7,16 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
 	private IntWritable result = new IntWritable();
-
+	
 	protected void reduce(Text key, Iterable<IntWritable> values,
 			Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
 		
 		int sum = 0;
-		
 		for (IntWritable val : values) {
 			sum += val.get();
 		}
-		
 		result.set(sum);
-		
 		context.write(key, result);
 	}
 }
